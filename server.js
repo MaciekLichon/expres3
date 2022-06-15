ce693a4a79a81ab485206007e24c1992d56c2a51
Maciej Lichoń <maciek.lichon@gmail.com>
2022-06-15 20:46:29 +0200

    Add remote database

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const seatsRoutes = require('./routes/seats.routes');
 
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/NewWaveDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://MaciekL:maciek@cluster0.1847h.mongodb.net/NewWaveDB?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.once('open', () => {
   console.log('Connected to the database');
